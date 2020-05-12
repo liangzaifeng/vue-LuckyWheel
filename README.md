3a94895f6eb30f14c876899729fe648febee1a5a
liyuanfeng <915749119@qq.com>
2020-05-12 11:39:47 +0800

    提交代码

---
+++ README.md
@@ -0,0 +1,92 @@
+## LuckyWheel props
+  |参数 | 说明 | 类型 | 是否必传 |
+  |:---------: | :-----------: | :------: | :------: |
+  |prizeList | 奖品列表 | Array | 必传 |
+  |asyncAjax | 异步发请求获取中奖请求函数 | Function | 必传 |
+  |isPointer | 是否要指针旋转 | Boolean | 非必传 |
+  
+<br>
+
+## LuckyWheel props
+  |方法名 | 说明 | 参数 | 
+  |:---------: | :------: | :------: | 
+  |onSuccess | 旋转停止后的回到 | params 中奖的下标 |
+
+<br>
+
+```
+<template>
+  <div id="app">
+    <LuckyWheel
+      :prizeList="prizeList"
+      :asyncAjax="asyncAjax"
+      isPointer
+      @onSuccess="onSuccess"
+    />
+  </div>
+</template>
+
+<script>
+import LuckyWheel from './components/LuckyWheel'; // 引入组件
+export default {
+  name: 'app',
+  data() {
+    return {
+      // 抽奖数据
+      prizeList: [
+        {
+          name: '奖品1',
+          img: require('./components/LuckyWheel/img/prize1.png')
+        },
+        {
+          name: '奖品2',
+          img: require('./components/LuckyWheel/img/prize2.png')
+        },
+        {
+          name: '奖品3',
+          img: require('./components/LuckyWheel/img/prize3.png')
+        },
+        {
+          name: '奖品4',
+          img: require('./components/LuckyWheel/img/prize4.png')
+        },
+        {
+          name: '奖品5',
+          img: require('./components/LuckyWheel/img/prize4.png')
+        },
+        {
+          name: '奖品6',
+          img: require('./components/LuckyWheel/img/prize5.png')
+        },
+        {
+          name: '奖品7',
+          img: require('./components/LuckyWheel/img/prize6.png')
+        },
+        {
+          name: '谢谢惠顾',
+          img: require('./components/LuckyWheel/img/prize7.png')
+        }
+      ]
+    };
+  },
+  methods: {
+    // 抽中后的回调
+    onSuccess(index) {
+      alert('恭喜您获得' + this.prizeList[index].name);
+    },
+    // 抽奖时发请求获取中奖下标
+    asyncAjax() {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          let num = Math.floor(Math.random() * this.prizeList.length);
+          resolve(num);
+        }, 1000);
+      });
+    }
+  },
+  components: {
+    LuckyWheel
+  }
+};
+</script>
+
---
+++ index.vue
@@ -0,0 +1,170 @@
+<template>
+  <div class="wheel-wrap" @transitionend="transitionend">
+    <div
+      class="wheel"
+      :style="{ transform, transition: `transform ${duration}s` }"
+    >
+      <div class="prize">
+        <div
+          class="prize-item"
+          v-for="(item, index) in prizeList"
+          :key="index"
+          :style="{
+            transform: `rotate(${(360 / prizeList.length) * index +
+              21}deg) translateY(-1.3rem)`
+          }"
+        >
+          <img :src="item.img" alt="" />
+          <p>{{ item.name }}</p>
+        </div>
+      </div>
+    </div>
+    <div
+      class="pointer"
+      @click="handleClick"
+      :style="{
+        transform: pointerTransform,
+        transition: `transform ${duration}s`
+      }"
+    >
+      <img src="./img/pointer.png" alt="" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'LuckWheel',
+  props: {
+    // 奖品列表
+    prizeList: {
+      type: Array,
+      default: () => []
+    },
+    // 异步发请求获取中奖请求
+    asyncAjax: {
+      type: Function,
+      default: null
+    },
+    // 是否要指针旋转
+    isPointer: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      transform: '', // 转盘旋转的样式
+      pointerTransform: '', // 指针旋转样式
+      cycles: 4, // 固定旋转几圈
+      pointercycles: 8, // 指针固定旋转几圈
+      preDeg: 0, // 上一次的旋转角度
+      flag: true, // 节流
+      winningIndex: undefined, // 中间的索引
+      duration: 3 // 旋转时间
+    };
+  },
+  methods: {
+    // 开始抽奖
+    async handleClick() {
+      if (!this.flag) return;
+      this.flag = false;
+      // 随机需要选中的索引
+      let winningIndex = await this.asyncAjax();
+      console.log('winningIndex:' + winningIndex);
+      this.winningIndex = winningIndex;
+      if (this.isPointer) {
+        this.pointerTransform = `rotate(${this.countDeg(winningIndex)}deg)`;
+      } else {
+        let randomIndex = this.prizeList.length - 1 - winningIndex;
+        this.transform = `rotate(${this.countDeg(randomIndex)}deg)`;
+      }
+    },
+
+    // 计算圈数
+    countDeg(index) {
+      // 旋转圈数
+      let cyclesDeg = this.isPointer ? this.pointercycles : this.cycles;
+      cyclesDeg *= 360;
+
+      // 旋转角度
+      let sumDeg =
+        (360 / this.prizeList.length) * index + 21 + cyclesDeg + this.preDeg;
+
+      // 初始化到原始点
+      this.preDeg =
+        (360 / this.prizeList.length) * (this.prizeList.length - index) -
+        21 +
+        sumDeg;
+      return sumDeg;
+    },
+    transitionend() {
+      this.flag = true;
+      this.$emit('onSuccess', this.winningIndex);
+    }
+  }
+};
+</script>
+<style>
+html {
+  font-size: 13.3333vw;
+}
+</style>
+<style lang="scss" scoped>
+.wheel-wrap {
+  position: relative;
+  display: inline-block;
+  overflow: hidden;
+
+  .wheel {
+    position: relative;
+    width: 6.26rem;
+    height: 6.26rem;
+    background: url('./img/dial.png') no-repeat center top;
+    background-size: 100%;
+
+    .prize {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      .prize-item {
+        position: absolute;
+        width: 1.6rem;
+        bottom: 0;
+        left: -0.8rem;
+        text-align: center;
+        transform-origin: bottom center;
+
+        p {
+          font-size: 0.12rem;
+          margin: 0;
+        }
+        img {
+          width: 1.3rem;
+          height: 0.8rem;
+          vertical-align: middle;
+        }
+      }
+    }
+  }
+}
+
+.pointer {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-left: -0.88rem;
+  margin-top: -0.9rem;
+  z-index: 2;
+  width: 1.67rem;
+  height: 1.67rem;
+
+  img {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 1.67rem;
+    height: 1.83rem;
+  }
+}
+</style>
